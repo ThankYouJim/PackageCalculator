9c81ebc94b03a7d02d1e97b7ea8ae7ff388b5346
Jo <jo3cyc@gmail.com>
2016-09-03 17:30:56 +0800

    Redone canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -1,82 +1,76 @@
-﻿// GLOBAL
+﻿/******************************************************************************
+ * CANVAS.JS - 
+ ******************************************************************************/
+ 
+ // TODO: redraw the canvas -> or resave an image and reload page to show the change.
+
+// GLOBAL
 var WIDTH = 400,
     HEIGHT = 300;
-var VIEW_ANGLE = 33,
+var container, scene, camera, renderer;
+var controls;
+// CAMERA
+var VIEW_ANGLE = 45,
     ASPECT = WIDTH / HEIGHT,
     NEAR = 0.1,
     FAR = 10000;
-var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-    camera.position.z = 100;
-var scene = new THREE.Scene();
-scene.add(camera);
 
-var mouse = {
-    x: 0,
-    y: 0
-};
-document.addEventListener('mousedown', onDocumentMouseDown, false);
+// CUSTOM
+var pkg, mCase;
+var axis;
 
-var renderer_P = new THREE.WebGLRenderer();
-renderer_P.setPixelRatio(window.devicePixelRatio);
-renderer_P.setSize(WIDTH, HEIGHT);
+init();
+render();
 
-var renderer_MC = new THREE.WebGLRenderer();
-renderer_MC.setPixelRatio(window.devicePixelRatio);
-renderer_MC.setSize(WIDTH, HEIGHT);
-
-var $canvas_P = $('#pkgCanvas');
-var $canvas_MC = $('#caseCanvas');
-$canvas_P.append(renderer_P.domElement);
-$canvas_MC.append(renderer_MC.domElement);
-
-//var box = new THREE.BoxGeometry(pkg_MET.w, pkg_MET.w, pkg_MET.h);
-//var box = new THREE.BoxGeometry(40, 40, 40);
-//var material = new THREE.MeshBasicMaterial({ wireframe: true });
-//mesh = new THREE.Mesh(box, material);
-//scene.add(mesh);
-//renderer_P.render(scene, camera);
-
-// TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING 
-
-// Materials
-var geometry = new THREE.BoxGeometry(5, 5, 5);
-var material = new THREE.MeshLambertMaterial({ color: 0xFF0000 });
-var mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-// Light source
-var light = new THREE.PointLight( 0xFFFF00 );
-light.position.set( 10, 0, 10 );
-scene.add( light );
-renderer_P.setClearColor(0xdddddd, 1);
-// To see the dark sides of the cube against the background, set the background color to grey.
-
-var red = new THREE.LineBasicMaterial({ color: "rgb(255, 0, 0)" });
-var green = new THREE.LineBasicMaterial({ color: "rgb(0, 255, 0)" });
-var blue = new THREE.LineBasicMaterial({ color: "rgb(0, 0, 255)" });
-
-
-var axisHelper = new THREE.AxisHelper(1);
-scene.add(axisHelper);
-
-
-
-// TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING 
-
-//(function () {
-    animate();
-//});
-
-    function onDocumentMouseDown(event) {
-        mouse.x = (event.clientX - WIDTH / 2);
-        mouse.y = (event.clientY - HEIGHT / 2);
-    };
-
-function animate() {
-    requestAnimationFrame(animate);
-
-    camera.position.x += (mouse.x - camera.position.x) * .05;
-    camera.position.y = THREE.Math.clamp(camera.position.y + (-mouse.y - camera.position.y) * .05, 0, 1000);
+function init() {
+    // SCENE
+    var scene = new THREE.Scene();
+    // Camera
+    camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+    camera.position.set(150, 150, 400);
     camera.lookAt(scene.position);
+    scene.add(camera);
+    // RENDERER
+    var renderer_P = new THREE.WebGLRenderer({ antialias: true });
+    renderer_P.setPixelRatio(window.devicePixelRatio);
+    renderer_P.setSize(WIDTH, HEIGHT);
 
+    var renderer_MC = new THREE.WebGLRenderer({ antialias: true });
+    renderer_MC.setPixelRatio(window.devicePixelRatio);
+    renderer_MC.setSize(WIDTH, HEIGHT);
+
+    var $canvas_P = $('#pkgCanvas');
+    $canvas_P.append(renderer_P.domElement);
+    var $canvas_MC = $('#caseCanvas');
+    $canvas_MC.append(renderer_MC.domElement);
+
+    // MESH & MATERIALS
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ wireframe: true });
+    pkg = new THREE.Mesh(geometry, material);
+    pkg.scale.set(pkg_MET.w, pkg_MET.w, pkg_MET.h);
+    scene.add(pkg);
+
+    //mCase = new THREE.Mesh(geometry, material);
+    //mCase.scale.set(case_MET.w, case_MET.d, case_MET.h);
+    //scene.add(mCase);
+
+    // AXIS
+    axis = new THREE.AxisHelper(50);
+    axis.position.x = -(PKG_DEFAULT.x + 30);  // Always slightly left of the shape
+
+    // CONTROLS
+    //controls = new THREE.OrbitControls(camera, renderer.domElement);
+    //controls.addEventListener('change', render);
+    //controls.enableDamping = true;
+    //controls.dampingFactor = 0.9;
+
+}
+
+function render() {
+    // To see the dark sides of the cube against the background, set the background color to grey.
+    //camera.lookAt(scene.position);
+    //renderer_P.setClearColor(0xdddddd, 1);
     renderer_P.render(scene, camera);
+    renderer_MC.render(scene, camera);
 }
--- js/form.js
+++ js/form.js
@@ -1,13 +1,10 @@
-//Const values
-var IMP_IMP = 2.54;
-var IMP_TO_IMP_CUBE = 0.028317;
-var INCHES = 12;
-var FT_CUBE = 1728;
-var M_CUBE = 1000000;
+/******************************************************************************
+ * FORM.JS - 
+ * All editable inputs will recalculated Imperial to Metric or vice versa on 
+ * their respective pair, and recalculates the cube.
+ ******************************************************************************/
 
-var mod = 0;
-var q1, q2;
-var pkg_MET = new Config(), pkg_IMP = new Config(), case_MET = new Config(), case_IMP = new Config();
+// CONST
 /* TODO: change to constant?
  * var constant = function(val) {
  *   return function() {
@@ -17,10 +14,31 @@ var pkg_MET = new Config(), pkg_IMP = new Config(), case_MET = new Config(), cas
  * a = constant(10);
  * a(); // 10
  */
+var MET_IMP = 2.54;
+var MET_TO_IMP_CUBE = 0.028317;
+var INCHES = 12;
+var FT_CUBE = 1728;
+var M_CUBE = 1000000;
 
-/******************************************************************
+// DEFAULT VALUES
+var DEFAULT = {
+    w: 20,
+    d: 10,
+    h: 23,
+    q1:3, q2: 2
+}
+
+// CUSTOM
+var mod = 0;
+var modValues = [1.5, 2, 2.5];
+var q1, q2;
+var pkg_MET, pkg_IMP, case_MET, case_IMP;
+
+init();
+
+/******************************************************************************
  * Math! Yay!
- ******************************************************************/
+ *****************************************************************************/
 // Round to 4 places max, but only when necessary.
 // The number of 0's determines no. of desired decimal places.
 function roundToNearest(num) {
@@ -38,17 +56,17 @@ function calcFTCube(w, d, h) {
 
 // Convert inch to cm
 function convertToMetric(val) {
-    return roundToNearest(val * IMP_IMP);
+    return roundToNearest(val * MET_IMP);
 }
 
 // Convert cm to inch
 function convertToImperial(val) {
-    return roundToNearest(val / IMP_IMP);
+    return roundToNearest(val / MET_IMP);
 }
 
-/******************************************************************
+/******************************************************************************
  * HTML Event Listeners and Modifiers
- ******************************************************************/
+ *****************************************************************************/
 // Object to store the package/case values
 function Config() {
     this.w = 0;
@@ -57,30 +75,51 @@ function Config() {
     this.cube = 0;
 }
 
-// On button clicked: change appeared modifier values
-// i) +1.5, +1.5, +2; or ii) +2, +2, +2.5
-var modValues = [1.5, 2, 2.5];
+function setQty(a, b) {
+    $('#qty1').val(a);
+    $('#qty2').val(b);
+}
+
+// On button clicked: change appeared modifier values (CM) = [1.5, 2, 2.5];
+// a) +1.5, +1.5, +2; or b) +2, +2, +2.5
 function setMod() {
     document.getElementById('optW').innerHTML = "+" + modValues[mod];
     document.getElementById('optD').innerHTML = "+" + modValues[mod];
     document.getElementById('optH').innerHTML = "+" + modValues[mod + 1];
-    mod = 1 - mod;
+    mod = 1 - mod; // so it'll only switch b/w modValues[0] or [1]
 
-    updateMCaseCube();
+    updateMCase();
+}
+
+function setMod_Sp() {
+    // TODO: As sometimes the padding of the master case might be +2, +2, +2 (CM)
 }
 
 // pass the value to the ele reference object
 function updateMetricCell(ele, num) {
     ele.val(convertToMetric(num));
 }
-// pass the value to the ele reference object
 function updateImperialCell(ele, num) {
     ele.val(convertToImperial(num));
 }
 
+function updateAll() {
+    pkg_IMP.w = updateImperialCell($('#pkg_IMP_w'), pkg_MET.w);
+    pkg_IMP.d = updateImperialCell($('#pkg_IMP_d'), pkg_MET.d);
+    pkg_IMP.h = updateImperialCell($('#pkg_IMP_h'), pkg_MET.h);
+    //pkg_MET.w = updateMetricCell($('#pkg_MET_w'), pkg_IMP.w);
+    //pkg_MET.d = updateMetricCell($('#pkg_MET_d'), pkg_IMP.d);
+    //pkg_MET.h = updateMetricCell($('#pkg_MET_h'), pkg_IMP.h);
+    case_IMP.w = updateImperialCell($('#case_IMP_w'), case_MET.w);
+    case_IMP.d = updateImperialCell($('#case_IMP_d'), case_MET.d);
+    case_IMP.h = updateImperialCell($('#case_IMP_h'), case_MET.h);
+    calcMCube();
+    calcFTCube();
+}
+
 // Calculates the case config values according to user input qty and modifier (above)
-// This is only in Metric as the modifiers are only considered in Metric
-function updateMCaseCube() {
+// This is only in Metric as the modifiers are only considered in Metric on China's side
+function updateMCase() {
 	var opt;
 	var q1 = $('#qty1').val();
 	var q2 = $('#qty2').val();
@@ -101,17 +140,34 @@ function updateMCaseCube() {
         // Cube
         case_MET.cube = calcMCube(case_MET.w, case_MET.d, case_MET.h);
         $('#case_MET_c').val(case_MET.cube);
-        case_MET.cube = calcMCube(case_MET.w, case_MET.d, case_MET.h);
+        case_IMP.cube = calcFTCube(case_IMP.w, case_IMP.d, case_IMP.h);
         $('#case_IMP_c').val(case_IMP.cube);
 	}
 }
 
-// All editable inputs will recalculated Imperial to Metric or vice versa on their respective pair.
-// And recalculates the cube.
-// TODO: redraw the canvas -> or resave an image and reload page to show the change.
+function setDefault() {
+    setQty(DEFAULT.q1, DEFAULT.q2);
+
+    $('#pkg_MET_w').val(DEFAULT.w);
+    pkg_MET.w = DEFAULT.w;
+    //pkg_IMP.w = updateImperialCell($('#pkg_IMP_w'), pkg_MET.w);
+
+    $('#pkg_MET_d').val(DEFAULT.d);
+    pkg_MET.d = DEFAULT.d;
+    //pkg_IMP.d = updateImperialCell($('#pkg_IMP_d'), pkg_MET.d);
+
+    $('#pkg_MET_h').val(DEFAULT.h);
+    pkg_MET.h = DEFAULT.h;
+    //pkg_IMP.h = updateImperialCell($('#pkg_IMP_h'), pkg_MET.h);
+
+    pkg_MET.cube = calcMCube(pkg_MET.w, pkg_MET.d, pkg_MET.h);
+    //pkg_IMP.cube = calcFTCube(pkg_IMP.w, pkg_IMP.d, pkg_IMP.h);
+
+    updateMCase();
+}
 
 // Below: JQuery version for the new 'oninput' listener
-//$("#myId").on('change keydown paste input', function () {
+// $("#myId").on('change keydown paste input', function () {
 //    doSomething();
 //});
 $('.cell').on('input', function () {
@@ -120,7 +176,7 @@ $('.cell').on('input', function () {
         // PACKAGE : METRIC
         case 'pkg_MET_w':
             pkg_MET.w = $('#pkg_MET_w').val();
-            pkg_IMP.w = updateImperialCell($('#pkg_IMP_w'), pkg_MET.w);
+            pkg_IMP.w = updateCell($('#pkg_IMP_w'), convertToImperial(pkg_MET.w));
             break;
         case 'pkg_MET_d':
             pkg_MET.d = $('#pkg_MET_d').val();
@@ -172,7 +228,7 @@ $('.cell').on('input', function () {
             break;
     }
 
-    // Upon getting all 3 figures, calculate the cube of the package and calculates master case
+    // Upon getting all 3 figures (on any side), calculate the cube of the package and calculates master case
     if ((pkg_MET.w != 0 && pkg_MET.d != 0 && pkg_MET.h != 0) || (pkg_IMP.w != 0 && pkg_IMP.d != 0 && pkg_IMP.h != 0)) {
         // Recalculates the cube when the inputs get changed
         pkg_MET.cube = calcMCube(pkg_MET.w, pkg_MET.d, pkg_MET.h);
@@ -182,27 +238,30 @@ $('.cell').on('input', function () {
         $('#pkg_IMP_c').val(pkg_IMP.cube);
         
         // Calculates the Master Case 
-    	  updateMCaseCube();
+    	  updateMCase();
     }
-
 });
 
+// Switch the two qty values and updates the master case
 $('#times').on('click', function () {
 	var temp;
 	temp = $('#qty1').val();
 	$('#qty1').val($('#qty2').val());
 	$('#qty2').val(temp);
 	
-	updateMCaseCube();
+	updateMCase();
 });
 
-function setQty() {
-    var div = document.getElementById('qtyCover')
-
-}
+// TODO: Show a qty count above the current qty form. 
+// Tried but didn't work last time
+//function setQty() {
+//    var div = document.getElementById('qtyCover')
+//}
 
 
 // init
-(function () {
+function init() {
+    pkg_MET = new Config(), pkg_IMP = new Config(), case_MET = new Config(), case_IMP = new Config();
+    setDefault();
     setMod();   // This has a delay than rest of the form. Uh?
-})();
+};
